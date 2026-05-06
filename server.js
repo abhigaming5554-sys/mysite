661768a42857fd31ca6fa2219b68320f9f8a0206
abhigaming5554-sys <abhigaming5554@gmail.com>
2026-05-06 13:34:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const fetch = require("node-fetch");
+const cors = require("cors");
 
 const app = express(); // ✅ YE MISSING THA
 
+app.use(cors());
 app.use(express.json());
 
 // ENV variables (Render से आएंगे)
